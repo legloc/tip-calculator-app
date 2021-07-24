3c8f4b4239af91149f9f7bb22abf5a188549942e
legloc <aliiev.akhtem@gmail.com>
2021-07-24 21:12:42 +0300

    Fixed computations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,26 +5,56 @@ import OutputGroup from './OutputGroup'
 
 const Calculator = () => {
   const [state, setState] = useState({
-    bill: 0,
-    tip: 0,
-    persons: 0
+    bill: null,
+    tip: null,
+    persons: null,
   })
+
+  const countTotal = () => {
+    let count = state.bill * state.tip * 0.01
+    return isNaN(count) ? 0 : count
+  }
+
+  const countTipAmount = () => {
+    let count = countTotal() / state.persons
+    return isNaN(count) ? 0 : count
+  }
 
   return (
     <div className="calculator">
 
       <div className="wrapper-input">
-        <Input title="Bill" icon="dollar" onChange={ e => setState({ ...state, bill: e }) } />
-        <RadioGroup title="Select tip %" name="radio-tip" onSelect={ e => setState({ ...state, tip: e }) } />
-        <Input title="Person" icon="person" onChange={ e => setState({ ...state, persons: e }) } />
+        <Input
+          title="Bill"
+          icon="dollar"
+          isValid={ state.bill !== 0 }
+          onChange={ e => setState({ ...state, bill: e  }) } />
+        
+        <RadioGroup
+          title="Select tip %"
+          name="radio-tip"
+          onSelect={ e => setState({ ...state, tip: parseInt(e) }) } />
+        
+        <Input
+          title="Person"
+          icon="person"
+          isValid={ state.persons !== 0 }
+          onChange={ e => setState({ ...state, persons: e }) } />
       </div>
 
       <div className="wrapper-output">
         <div>
-          <OutputGroup title="Tip amount" amount={ state.bill * state.tip * 0.01 / state.persons } />
-          <OutputGroup title="Total" amount={ state.bill * state.tip * 0.01 } />
+          <OutputGroup
+            title="Tip amount"
+            amount={ countTipAmount() } />
+          
+          <OutputGroup
+            title="Total"
+            amount={ countTotal() } />
         </div>
-        <button className="btn-reset" onClick={ () => setState({ ...state, bill: 1 }) }>Reset</button>
+        <button
+          className="btn-reset"
+          onClick={ () => setState({ bill: 0, tip: 0, persons: 0 }) }>Reset</button>
       </div>
 
     </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,13 @@
-const Input = ({ title, icon, onChange }) => {
+const Input = ({ title, icon, isValid, onChange }) => {
   return (
-    <div className="input-group">
+    <div className={ `input-group ${ !isValid && "input-error" }` }>
       <label className="group-wrapper">
-        <span className="group-title">{ title }</span>
+        <div className="group-header">
+          <span className="group-title">{ title }</span>
+          <span className="group-error">Can't be a zero</span>
+        </div>
         <div className={ `input-wrapper icon-${ icon }` }>
-          <input type="text" placeholder="0" onChange={ e => onChange(e.target.value) } />
+          <input type="number" placeholder="0" min="0" onChange={ e => onChange(parseInt(e.target.value)) } />
         </div>
       </label>
     </div>
